8a8735c9a1bcafe776ad71d3b83e3c91a3800975
Tristan Himmelman <tristan@branchout.com>
2014-10-16 13:48:31 -0400

    Created Xcode framework project

---
+++ ObjectMapper/ObjectMapper/ObjectMapper.h
@@ -0,0 +1,19 @@
+//
+//  ObjectMapper.h
+//  ObjectMapper
+//
+//  Created by Tristan Himmelman on 2014-10-16.
+//  Copyright (c) 2014 hearst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjectMapper.
+FOUNDATION_EXPORT double ObjectMapperVersionNumber;
+
+//! Project version string for ObjectMapper.
+FOUNDATION_EXPORT const unsigned char ObjectMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectMapper/PublicHeader.h>
+
+
