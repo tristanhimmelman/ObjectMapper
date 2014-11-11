cf68b04219195c03dd0add98d946cb2be66287db
Coen Wessels <c.dewessels@gmail.com>
2014-11-11 13:23:36 +0100

    Made methods in transforms public

--- ObjectMapper/Transforms/DateTransform.swift
+++ ObjectMapper/Transforms/DateTransform.swift
@@ -14,14 +14,14 @@ public class DateTransform<ObjectType, JSONType>: MapperTransform<ObjectType, JS
         
     }
     
-    override func transformFromJSON(value: AnyObject?) -> ObjectType? {
+    override public func transformFromJSON(value: AnyObject?) -> ObjectType? {
         if let timeInt = value as? Double {
             return (NSDate(timeIntervalSince1970: NSTimeInterval(timeInt)) as ObjectType)
         }
         return nil
     }
     
-    override func transformToJSON(value: ObjectType?) -> JSONType? {
+    override public func transformToJSON(value: ObjectType?) -> JSONType? {
         if let date = value as? NSDate {
             return (Double(date.timeIntervalSince1970) as JSONType)
         }
