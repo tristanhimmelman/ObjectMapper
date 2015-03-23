dbc149cd94670fce3f61f1a9329acfd3cd121bb0
Syo Ikeda <suicaicoca@gmail.com>
2015-03-23 19:45:41 +0900

    Add OS X target

--- ObjectMapper/ObjectMapper.h
+++ ObjectMapper/ObjectMapper.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 hearst. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for ObjectMapper.
 FOUNDATION_EXPORT double ObjectMapperVersionNumber;
