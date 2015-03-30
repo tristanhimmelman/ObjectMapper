8f2b82c1d06659d4f3e788952a7e663d4f5fb478
Syo Ikeda <suicaicoca@gmail.com>
2015-03-31 07:07:32 +0900

    Indent

--- ObjectMapper/Core/FromJSON.swift
+++ ObjectMapper/Core/FromJSON.swift
@@ -9,26 +9,26 @@
 internal final class FromJSON {
 	
 	/// Basic type
-    class func basicType<FieldType>(inout field: FieldType, object: FieldType?) {
-        if let value = object {
-            field = value
-        }
-    }
-	
+	class func basicType<FieldType>(inout field: FieldType, object: FieldType?) {
+		if let value = object {
+			field = value
+		}
+	}
+
 	/// optional basic type
-    class func optionalBasicType<FieldType>(inout field: FieldType?, object: FieldType?) {
-        if let value = object {
-            field = value
-        }
-    }
-	
+	class func optionalBasicType<FieldType>(inout field: FieldType?, object: FieldType?) {
+		if let value = object {
+			field = value
+		}
+	}
+
 	/// Implicitly unwrapped optional basic type
 	class func optionalBasicType<FieldType>(inout field: FieldType!, object: FieldType?) {
 		if let value = object {
 			field = value
 		}
 	}
-	
+
 	/// Mappable object
 	class func object<N: Mappable>(inout field: N, object: AnyObject?) {
 		if let value: N = Mapper().map(object) {
