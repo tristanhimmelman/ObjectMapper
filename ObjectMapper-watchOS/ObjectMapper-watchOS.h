cdad49e8d08b2981b2dcfdd65cb8f67550bdf26f
Tristan Himmelman <tristanhimmelman@gmail.com>
2015-06-13 12:33:26 -0400

    added watchOS target

---
+++ ObjectMapper-watchOS/ObjectMapper-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  ObjectMapper-watchOS.h
+//  ObjectMapper-watchOS
+//
+//  Created by Tristan Himmelman on 2015-06-13.
+//  Copyright © 2015 hearst. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for ObjectMapper-watchOS.
+FOUNDATION_EXPORT double ObjectMapper_watchOSVersionNumber;
+
+//! Project version string for ObjectMapper-watchOS.
+FOUNDATION_EXPORT const unsigned char ObjectMapper_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjectMapper_watchOS/PublicHeader.h>
+
+
