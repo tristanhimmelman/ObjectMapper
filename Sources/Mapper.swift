c045dc1fc1b719c45618f5f387aa004dfc3a8bc6
Suyeol Jeon <devxoul@gmail.com>
2017-03-21 16:27:16 +0900

    Add support to ImmutableMappable can become a property of Mappable

--- Sources/Mapper.swift
+++ Sources/Mapper.swift
@@ -106,8 +106,22 @@ public final class Mapper<N: BaseMappable> {
 				object.mapping(map: map)
 				return object
 			}
-		} else if N.self is ImmutableMappable.Type { // Check if object is ImmutableMappable
-			assert(false, "'ImmutableMappable' type requires throwing version of function \(#function)  - use 'try' before \(#function)")
+		} else if let klass = N.self as? ImmutableMappable.Type { // Check if object is ImmutableMappable
+			do {
+				return try klass.init(map: map) as? N
+			} catch let error {
+				#if DEBUG
+				let exception: NSException
+				if let mapError = error as? MapError {
+					exception = NSException(name: .init(rawValue: "MapError"), reason: mapError.description, userInfo: nil)
+				} else {
+					exception = NSException(name: .init(rawValue: "ImmutableMappableError"), reason: error.localizedDescription, userInfo: nil)
+				}
+				exception.raise()
+				#else
+				NSLog("\(error)")
+				#endif
+			}
 		} else {
 			// Ensure BaseMappable is not implemented directly
 			assert(false, "BaseMappable should not be implemented directly. Please implement Mappable, StaticMappable or ImmutableMappable")
