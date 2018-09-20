4d94af898fbf7d3dced4b66aac7faef33c8e8dcf
Victor Hugo Barros <hey.zooi@gmail.com>
2018-09-20 11:35:43 -0700

    adding support for Swift 4.2 without break support for older versions of Swift 4 and Swift 3

--- Package.swift
+++ Package.swift
@@ -1,9 +1,5 @@
-// swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "ObjectMapper",
-    swiftLanguageVersions = [.v3, .v4, .v4_2, .version("5")]
+    name: "ObjectMapper"
 )
---
+++ Package@swift-4.swift.swift
@@ -0,0 +1,9 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ObjectMapper",
+    swiftLanguageVersions = [3, 4]
+)
