b0d3225eed0dd4edede1103b42f97bf1dd23abd9
Victor Hugo Barros <heyzooi@users.noreply.github.com>
2018-09-19 17:06:35 -0700

    Update Package.swift
    Updating Package.swift to have a better support for Swift 4.2

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "ObjectMapper"
+    name: "ObjectMapper",
+    swiftLanguageVersions = [.v3, .v4, .v4_2, .version("5")]
 )
