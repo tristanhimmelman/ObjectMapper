e26443d5b57d5efa180972a1e934d52db7658b0c
Konstantin Loginov <loginov.k@gmail.com>
2021-02-18 09:18:13 +0100

    Fixing build-failure in Xcode 12.5

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
+// swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(
-    name: "ObjectMapper",
-    targets: [
-        .target(
-            name: "ObjectMapper",
-            path: "Sources"
-        )
-    ]
-)
+let package = Package(name: "ObjectMapper",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v10),
+                                  .tvOS(.v9),
+                                  .watchOS(.v2)],
+                      products: [.library(name: "ObjectMapper",
+                                          targets: ["ObjectMapper"])],
+                      targets: [.target(name: "ObjectMapper",
+                                        path: "Sources")],
+                      swiftLanguageVersions: [.v5])
